6b37a176191b417a5d2b0f8dff25c071d06f3c3e
wss <wss@e-lead.cn>
2025-04-15 16:06:44 +0800

    consul-consumer

--- consul-consumer/src/main/java/com/oneluffy/consumer/ConsulConsumerApplication.java
+++ consul-consumer/src/main/java/com/oneluffy/consumer/ConsulConsumerApplication.java
@@ -2,12 +2,15 @@ package com.oneluffy.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import com.oneluffy.consumer.config.MysqlConfig;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableConfigurationProperties({MysqlConfig.class})
 public class ConsulConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsulConsumerApplication.class, args);
---
+++ consul-consumer/src/main/java/com/oneluffy/consumer/controller/ConfigController.java
@@ -0,0 +1,34 @@
+package com.oneluffy.consumer.controller;
+
+import com.oneluffy.consumer.config.MysqlConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/config")
+public class ConfigController {
+
+    private final MysqlConfig mysqlConfig;
+
+    public ConfigController(MysqlConfig mysqlConfig) {
+        this.mysqlConfig = mysqlConfig;
+    }
+
+    @GetMapping("/mysql")
+    public Map<String, Object> getMysqlConfig() {
+        log.info("Current MySQL config: {}", mysqlConfig);
+        Map<String, Object> config = new HashMap<>();
+        config.put("url", mysqlConfig.getUrl());
+        config.put("username", mysqlConfig.getUsername());
+        config.put("password", mysqlConfig.getPassword());
+        config.put("driverClassName", mysqlConfig.getDriverClassName());
+        return config;
+    }
+} 
