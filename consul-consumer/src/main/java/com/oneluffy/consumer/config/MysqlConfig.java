9ced1655eff9864b8e1b7258d2fed35cea8ffdbd
wss <wss@e-lead.cn>
2025-04-15 16:08:09 +0800

    consul-consumer

---
+++ consul-consumer/src/main/java/com/oneluffy/consumer/config/MysqlConfig.java
@@ -0,0 +1,18 @@
+package com.oneluffy.consumer.config;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@RefreshScope
+@Configuration
+@ConfigurationProperties(prefix = "mysql")
+public class MysqlConfig {
+    private String url;
+    private String username;
+    private String password;
+    private String driverClassName;
+} 
