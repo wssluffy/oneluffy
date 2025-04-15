8c9c9e8912f5b1a25a1f41208b837974c9526569
wss <wss@e-lead.cn>
2025-04-15 15:05:19 +0800

    update

---
+++ consul-consumer/src/main/java/com/oneluffy/consumer/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.oneluffy.consumer.controller;
+
+import com.oneluffy.consumer.feign.ConsulServerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    private final ConsulServerClient consulServerClient;
+
+    public TestController(ConsulServerClient consulServerClient) {
+        this.consulServerClient = consulServerClient;
+    }
+
+    @GetMapping("/consumer/test")
+    public String test() {
+        return consulServerClient.test();
+    }
+} 
---
+++ consul-consumer/src/main/java/com/oneluffy/consumer/feign/ConsulServerClient.java
@@ -0,0 +1,11 @@
+package com.oneluffy.consumer.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "consul-server")
+public interface ConsulServerClient {
+    
+    @GetMapping("/consul/server")
+    String test();
+} 
