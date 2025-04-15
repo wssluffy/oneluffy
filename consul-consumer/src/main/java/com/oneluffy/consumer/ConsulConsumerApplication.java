de7c5fb93eea6e39913f08d42bafcc981f8cbee8
wss <wss@e-lead.cn>
2025-04-15 16:11:53 +0800

    consul-consumer

--- consul-consumer/src/main/java/com/oneluffy/consumer/ConsulConsumerApplication.java
+++ consul-consumer/src/main/java/com/oneluffy/consumer/ConsulConsumerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import com.oneluffy.consumer.config.MysqlConfig;
 
 @SpringBootApplication
 @EnableDiscoveryClient
